2bd8f4223204aa7742bc673530249ee31d24b324
Robert Gabriel Aghenitei <robert.aghenitei@gmail.com>
2025-05-07 22:02:38 +0300

    made the commit function (snapshot)

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import hashlib
 import os
 import pickle
+from pprint import pprint
+
+directory = os.getcwd() # This is the current directory
 
 def init_vcs():
-    os.makedirs(".my_Git", exist_ok=True)
+    os.makedirs(".my_git", exist_ok=True)
     print('VCS initialized.')
 
-# cwd = os.getcwd() // This is the current directory
+
 # print(cwd)  
 
 
@@ -16,8 +19,9 @@ def snapshot(directory):
 
     for (root, dir, files) in os.walk(directory, topdown=True):
         for file in files:
-            if '.my_Git' in os.path.join(root, file):
-                continue # ignores the files already in .my_Git (previous snapshots)
+            if any(skip_dir in os.path.join(root, file) for skip_dir in ['.my_git', '.git']):
+                continue  # ignores files in .my_Git or .git directories
+        
             file_path = os.path.join(root, file)
 
             with open(file_path, "rb") as f:
@@ -27,3 +31,18 @@ def snapshot(directory):
 
     hash_digest = snapshot_hash.hexdigest()
     snapshot_data['file_list'] = list(snapshot_data['files'].keys())
+
+    with open(f'.my_git/{hash_digest}', 'wb') as f:
+        pickle.dump(snapshot_data, f)
+    
+    pprint(f'Snapshot created with hash {hash_digest}')
+
+
+def revert_to_snapshot(hash_digest):
+    snapshot_path = f'.my_git/{hash_digest}'
+    if not os.path.exists(snapshot_path):
+        print("Snapshot does not exist")
+
+init_vcs()
+
+snapshot(directory)
